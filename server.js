913ddc42131979765f749dcc57275e16f9495a4a
Serena Lertkantitham <lertkantitham@gmail.com>
2019-01-26 18:41:24 -0800

    post request for recipes

--- server.js
+++ server.js
@@ -52,6 +52,22 @@ app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
 
+app.post('/recipes', jsonParser, (req, res) => {
+  // ensure 'name' and 'ingredients' are in request body
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message)
+      return res.status(400).send(message);
+    }
+  }
+
+  const recipe = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(200).json(recipe);
+})
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+})
